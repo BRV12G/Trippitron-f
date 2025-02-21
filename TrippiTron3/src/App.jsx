43d2b5dd4d5126ddcda02900d5d423e88e283128
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-02-21 22:52:01 +0530

    feat: Installed and implemented a Shadcn button

--- TrippiTron3/src/App.jsx
+++ TrippiTron3/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold underline text-gray-500 bg-red-500 mt-10 ml-5'>Welcome</h1>
+      <h1 className='text-3xl font-bold underline text-gray-500 bg-red-100 mt-10 ml-5'>Welcome</h1>
+      <Button>Button</Button>
     </>
   )
 }
