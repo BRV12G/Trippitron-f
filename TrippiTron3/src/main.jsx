3b251621dda3e0404a1dccbda17887795676a27b
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-02-21 23:34:26 +0530

    feat(router): add /create-trip route - Added a new route for creating trips. - Updated routing configuration in main.jsx

--- TrippiTron3/src/main.jsx
+++ TrippiTron3/src/main.jsx
@@ -2,16 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import CreateTrip from './create-trip/index.jsx'
 // import { RouterProvider } from 'react-router'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+  },
+  {
+    path: '/create-trip',
+    element: <CreateTrip />,
   }
+
 ])
 
 createRoot(document.getElementById('root')).render(
