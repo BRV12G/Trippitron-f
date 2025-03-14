96d144bc137d78768b7c38994d520f5b5b27bbf2
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-03-15 03:18:36 +0530

    updated the placeholder with actual images for itinerary

--- TrippiTron3/src/view-trip/components/PlaceCardItem.jsx
+++ TrippiTron3/src/view-trip/components/PlaceCardItem.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types';
 import { Button } from '@/components/ui/button';
 import { FaMapLocationDot } from "react-icons/fa6";
+import { GetPlaceDetails, PHOTO_REF_URL } from '@/service/GlobalApi';
 
 
 function PlaceCardItem({place}) {
+
+
+   const [photoUrl,setPhotoUrl]=useState();
+    useEffect(()=>{
+    place&&GetPlacePhoto();
+    }, [place])
+  
+    const GetPlacePhoto=async()=>{
+      const data={
+        textQuery:place.placeName
+      }
+      const result=await GetPlaceDetails(data).then(resp=>{
+        console.log(resp.data.places[0].photos[4].name);
+  
+        const PhotoUrl=PHOTO_REF_URL.replace('{NAME}', resp.data.places[0].photos[1].name);
+        setPhotoUrl(PhotoUrl)
+      })
+    }
+
   return (
     <div className='border rounded-xl p-3 flex gap-5 mt-2 hover:scale-105 transition-all hover:shadow-lg cursor-pointer'>
-      <img src='/travel_location11.jpg'alt={place.placeName}
+      <img src={photoUrl} alt={place.placeName}
             className='w-[200px] h-[150px]  rounded-xl shadow-lg'/>
        <div className=''>
          <h3 className='font-bold text-lg'>{place.placeName}</h3>
