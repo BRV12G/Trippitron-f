585630551b8ef7475bdbc3dbfeae5a980a33eb59
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-03-14 18:33:16 +0530

    feat:Hotel Recommendations Information and onclick directed to maps

--- TrippiTron3/src/view-trip/components/Hotels.jsx
+++ TrippiTron3/src/view-trip/components/Hotels.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from "prop-types";
+import { Link } from 'react-router-dom';
 
 
 function Hotels({trip}) {
@@ -8,7 +9,10 @@ function Hotels({trip}) {
       <h2 className='font-bold text-xl mt-5'>Hotel Recommendation</h2>
       <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-5'>
         {trip?.tripData?.hotelOptions?.map((hotel,index)=>(
-            <div key={hotel.id} className='hover:scale-105 transition-all cursor-pointer'>
+            <Link key={hotel.id} to={'https://www.google.com/maps/search/?api=1&query='+hotel.hotelName+","+ hotel?.hotelAddress} target='_blank'>
+            {/* // <Link key={hotel.id} to={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(`${hotel?.hotelName}, ${hotel?.hotelAddress}`)}`} */}
+            {/* // target="_blank" rel="noopener noreferrer"> */}
+            <div  className='hover:scale-105 transition-all cursor-pointer'>
                 <img src='/travel_location13.jpg' className='rounded-xl'/>
                 <div className='my-2 flex flex-col gap-2'>
                     <h2 className='font-medium'>{hotel?.hotelName}</h2>
@@ -18,6 +22,7 @@ function Hotels({trip}) {
                     <h2 className='text-gray-400'>{hotel?.description}</h2>
                 </div>
             </div>
+            </Link>
         ))}
       </div>
     </div>
