5f52b1d806e1a9d6e594d30ce902bc87824a5712
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-03-15 17:40:29 +0530

    feat: my trips page implemented

---
+++ TrippiTron3/src/my-trips/index.jsx
@@ -0,0 +1,47 @@
+import { db } from '@/service/firebaseConfig';
+import { collection, getDocs, where } from 'firebase/firestore';
+import React, { useEffect, useState }  from 'react'
+import { useNavigation } from 'react-router-dom';
+import { query } from 'firebase/firestore';
+import UserTripCardItem from './components/UserTripCardItem';
+
+function MyTrips() {
+  const navigation=useNavigation();
+  const [userTrips,setUserTrips]=React.useState([]);
+
+    useEffect(()=>{
+        GetUserTrips();
+    },[])
+    const GetUserTrips=async()=>{
+        const user=JSON.parse(localStorage.getItem('user'));
+        if(!user){
+            navigation('/');
+            return;
+        }
+        setUserTrips([]);
+        const q=query(collection(db,'AITrips'),where('userEmail','==', user?.email));
+        const querySnapshot = await getDocs(q);
+//         querySnapshot.forEach((doc) => {
+//         // doc.data() is never undefined for query doc snapshots
+//        console.log(doc.id, " => ", doc.data());
+//        setUserTrips(prevVal=>[...prevVal, doc.data()])
+// });
+const trips = querySnapshot.docs.map((doc) => doc.data());
+setUserTrips(trips);
+
+
+    }
+  return (
+    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-5 mb-5 '>
+      <h2 className='font-bold text-3xl'>My Trips</h2>
+
+      <div className='grid grid-cols-2 md:grid-cols-3  gap-5 mt-5'>
+        {userTrips.map((trip,index)=>(
+          <UserTripCardItem key={trip.id} trip={trip} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default MyTrips
